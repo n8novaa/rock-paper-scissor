02afc44e15f659d424c7ba2e9aa0d15d4ea6d89e
n8novaa <basilpaul71136@gmail.com>
2024-12-15 07:23:03 -0500

    create human input outcome

--- scripts.js
+++ scripts.js
@@ -6,4 +6,20 @@ function getComputerChoice()
     return choice[randomIndex];
 }
 
+function getHumanChoice()
+{
+
+    const choice = [ "rock", "paper", "scissor" ];
+    const userInput = prompt("enter the required choice from rock paper scissor").toLowerCase();
+    if (choice.includes(userInput))
+    {
+        return userInput;
+    }
+    else
+    {
+       alert("invalid choice enter rock paper or scissor");
+       return getHUmanChoice();
+    }
+}
+console.log(getHumanChoice());
 console.log(getComputerChoice());
