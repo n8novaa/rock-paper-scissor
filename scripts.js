4557a5df85fa96c7554511f86b416778e1928fe7
n8novaa <basilpaul71136@gmail.com>
2024-12-15 06:25:08 -0500

    create system generated outcome

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-prompt("hello world")
+function getComputerChoice()
+{
+    
+    const choice = [ "rock", "paper", "scissor" ];
+    const randomIndex =  Math.floor(Math.random() * choice.length);
+    return choice[randomIndex];
+}
+
+console.log(getComputerChoice());
